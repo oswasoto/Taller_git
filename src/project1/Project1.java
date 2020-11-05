9395c5cb781d5e9f7282569db49f8a7c6d0b4009
oswasoto <oswasoto@espol.edu.ec>
2020-11-05 18:51:07 -0500

    Hola mundo2

--- src/project1/Project1.java
+++ src/project1/Project1.java
@@ -15,7 +15,8 @@ public class Project1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo2");
     }
     
 }
